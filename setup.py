f6ac34804e344c96af614c20635b0fd74fa9d5a8
Abhiroop Tejomay <abhirooptejomay@gmail.com>
2021-04-05 16:44:45 +0000

    modified name and requirements

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='neural-style-transfer',
+    name='neural_style_transfer',
     version='0.1.0',
     description='Implementation of Neural Style Transfer using PyTorch',
     author='Abhiroop Tejomay',
     author_email='abhirooptejomay@gmail.com',
     url='https://github.com/visualCalculus/neural-style-transfer',
-    install_requires=['torch', 'torchvision', 
-                    'numpy', 'matplotlib', 
+    install_requires=['torch==1.7.0', 'torchvision==0.8.0', 
+                    'numpy==1.19.5', 'matplotlib', 
                     'pillow', 'tqdm'],
     packages=find_packages(),
 )

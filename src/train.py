c0a82366cdcab11d355a8e2eae2396f7674ecbc7
Abhiroop Tejomay <abhiroop@onwardhealth.co>
2021-04-05 12:46:30 +0000

    added comments and docstrings

--- src/train.py
+++ src/train.py
@@ -10,6 +10,7 @@ from torchvision import transforms
 from torchvision.models import vgg19
 
 from PIL import Image
+import matplotlib.pyplot as plt
 
 from models.vgg19 import VGG19
 from losses import ContentLoss, StyleLoss
@@ -19,42 +20,53 @@ from tqdm import tqdm
 def main(input_image="content"):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     print(f"training on device {device}")
+
+    # content and style images
     content_dir = "../images/content/neckarfront.jpg"
     style_dir = "../images/style/kandinsky.jpg"
     content = image_loader(content_dir, device)
     style = image_loader(style_dir, device)
+
+    # input image
     if input_image == "content":
         x = content.clone()
     elif input_image == "style":
         x = style.clone()
     else:
         x = torch.randn(content.data.size(), device=device)
-    torch.save(x.detach().to("cpu"), "../result/before_new.pt")
+
+    # mean and std for vgg19
     mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
     std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+    # vgg19 model
     model = VGG19(mean=mean, std=std).to(device=device)
     model = load_vgg19_weights(model)
     optimizer = optim.LBFGS([x.requires_grad_()])
-    # optimizer = optim.Adam([x.requires_grad_()])
 
+    # computing content and style representations
     content_outputs = model(content)
     style_outputs = model(style)
+
+    # defining content and style losses
     content_loss = ContentLoss(content_outputs["conv4"][1], device)
     style_losses = []
     for i in range(1, 6):
         style_losses.append(StyleLoss(style_outputs[f"conv{i}"][0], device))
 
+    # run style transfer
     output = train(model, optimizer, content_loss, style_losses, content, style, x)
-    torch.save(output.detach().to("cpu"), "../result/result_new.pt")
+    output = output.detach().to("cpu")
+
+    # save result
+    plt.imsave("../result/result.jpg", output[0].permute(1, 2, 0))
+    # torch.save(output.detach().to("cpu"), "../result/result_new.pt")
 
 def image_loader(path, device=torch.device("cuda")):
+    """Loads and resizes the image."""
     transform = transforms.Compose([
                     transforms.Resize((512, 512)),
                     transforms.ToTensor(),
-                    #transforms.Normalize(
-                    #    mean=(0.485, 0.456, 0.406),
-                    #    std = (0.229, 0.224, 0.225)
-                    #)
                 ])
     img = Image.open(path)
     img = transform(img)
@@ -62,7 +74,7 @@ def image_loader(path, device=torch.device("cuda")):
     return img
 
 def load_vgg19_weights(model):
-
+    """Loads VGG19 pretrained weights from ImageNet for style transfer"""
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     pretrained_model = vgg19(pretrained=True).features.to(device).eval()
 
@@ -117,7 +129,8 @@ def load_vgg19_weights(model):
 
 
 def train(model, optimizer, content_loss, style_losses, 
-          content, style, x, iterations=100, alpha=1, beta=1000000, style_weight=1):
+          content, style, x, iterations=100, alpha=1000, beta=1000000, style_weight=1):
+    """Train the neural style transfer algorithm."""
 
     with tqdm(range(iterations)) as iterations:
         for iteration in iterations:
@@ -125,19 +138,25 @@ def train(model, optimizer, content_loss, style_losses,
 
             def closure():
                 optimizer.zero_grad()
+
+                # correcting to 0-1 range
                 x.data.clamp_(0, 1)
                 outputs = model(x)
+
+                # input content and style representations
                 content_feature_maps = outputs["conv4"][1]
                 style_feature_maps = []
                 for i in range(1, 6):
                     style_feature_maps.append(outputs[f"conv{i}"][0])
 
+                # input content and style losses
                 total_content_loss = content_loss(content_feature_maps)
 
                 total_style_loss = 0
                 for feature_map, style_loss in zip(style_feature_maps, style_losses):
                     total_style_loss += (style_weight * style_loss(feature_map))
 
+                # total loss
                 loss = (alpha * total_content_loss) + (beta * total_style_loss)
                 loss.backward()
 
@@ -149,15 +168,15 @@ def train(model, optimizer, content_loss, style_losses,
 
                 return loss
 
-
             optimizer.step(closure)
 
+    # final correction
     x.data.clamp_(0, 1)
-
     return x
 
 
 if __name__ == "__main__":
+    # choose input image
     input_image = "content"
     main(input_image)
 

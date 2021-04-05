0dbe046ddeeb726c19f82c74277b3affe633751a
Abhiroop Tejomay <abhiroop@onwardhealth.co>
2021-04-05 14:56:55 +0000

    added setup details

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
-    version='0.0.0',
-    description='Describe Your Cool Project',
-    author='',
-    author_email='',
-    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
-    install_requires=['pytorch-lightning'],
+    name='neural-style-transfer',
+    version='0.1.0',
+    description='Implementation of Neural Style Transfer using PyTorch',
+    author='Abhiroop Tejomay',
+    author_email='abhirooptejomay@gmail.com',
+    url='https://github.com/visualCalculus/neural-style-transfer',
+    install_requires=['torch', 'torchvision', 
+                    'numpy', 'matplotlib', 
+                    'pillow', 'tqdm'],
     packages=find_packages(),
 )
 
